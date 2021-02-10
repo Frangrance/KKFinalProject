68c816bf6c1539de601dcfd51d0a8cf31be69151
Kutay Keleş <45495735+Frangrance@users.noreply.github.com>
2021-02-10 20:21:34 +0300

    10 ŞUBAT Core katmanı eklendi, dto eklendi.

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,15 +1,14 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
-using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccesss
 {
     //class : refarans tip olabilir.
     //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
     //new() : new lenebilir olmalı.
-    public interface IEntityRepository<T> where T:class,IEntity,new()
+    public interface IEntityRepository<T> where T:class, IEntity, new()
     {
         List<T> GetAll(Expression<Func<T,bool>> filter=null); //filtre vermediyse tüm data
         T Get(Expression<Func<T,bool>> filter);
